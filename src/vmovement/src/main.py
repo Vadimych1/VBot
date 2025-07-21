580f9f171afa9ea16aa930c969e9eecaf5c75bee
Vadimych1 <vadimlebedenko0@gmail.com>
2025-07-21 12:11:16 +0300

    Pathfinder fixes & movement update

--- src/vmovement/src/main.py
+++ src/vmovement/src/main.py
@@ -2,11 +2,25 @@ from miniros import AsyncROSClient
 from miniros.util.datatypes import Vector
 from miniros.util.decorators import decorators
 from miniros_vslam.source.datatypes import SLAMPosition
+from miniros_vmovement.source.control import TrackedRobotIK, MotorController, SerialReader
 import asyncio
+import numpy as np
+import platform
+import serial_asyncio as serial
+
+
+DRIVE_WHEEL_RADIUS = 0.02 # m
+TRACKS_SPACE = 0.14 # m
+MAX_RPM = 330 # rpm
+MAX_LINEAR_SPEED = 2 # m/s
+MAX_ANGULAR_SPEED = 25.4 # rad/s
+
 
 class VMovementClient(AsyncROSClient):
-    def __init__(self, ip = "localhost", port = 3000):
+    def __init__(self, controller: MotorController, ip = "localhost", port = 3000):
         super().__init__("vmovement", ip, port)
+
+        self.controller = controller
 
         self.s_pos = (0, 0)
         self.s_rot = 0
@@ -14,31 +28,50 @@ class VMovementClient(AsyncROSClient):
         self.m_target = None
         self.m_moving = False
 
+
     @decorators.aparsedata(Vector, 1)
     async def on_moveto(self, data: Vector, node: str):
         if data.y == 1:
             self.m_moving = False
             self.m_target = None
             
-            # TODO: Stop
-
+            self.controller.stop()
+            
         else:
             self.m_target = (data.x, data.z)
             self.m_moving = True
+
+            self.controller.set_position(*self.s_pos, self.s_rot) # TODO: check is s_rot degrees or radians
+    
+            await self.controller.move_to(
+                *self.m_target, 
+                high_precision = np.linalg.norm([self.s_pos[0] - self.m_target[0], self.s_pos[1] - self.m_target[1]]) < 0.5
+            )
+                
 
     @decorators.aparsedata(SLAMPosition, 1)
     async def on_vslam_pos(self, data: SLAMPosition):
         self.s_pos = (data.pos.x, data.pos.z)
         self.s_rot = data.ang.y
 
-    @staticmethod
-    def _calculate_motors_speed(go_from: tuple[float, float], go_to: tuple[float, float]):
-        # TODO: algos
-        ...
-
 
 async def main():
-    client = VMovementClient()
+    kinematics = TrackedRobotIK(
+        TRACKS_SPACE,
+        MAX_RPM,
+        DRIVE_WHEEL_RADIUS
+    )
+    
+    loop = asyncio.get_event_loop()
+    serial_port = "COM6" if platform.system() == "Windows" else "/ttyUSBmotor"
+    transport, protocol = await serial.create_serial_connection(loop, lambda: SerialReader(controller), serial_port, baudrate=9600)
+
+    controller = MotorController(kinematics, transport, MAX_RPM, MAX_LINEAR_SPEED, MAX_ANGULAR_SPEED)
+    
+    await asyncio.sleep(3.5) # wait for initialization of arduino
+    
+    client = VMovementClient(controller)
+
     await client.run()
 
 
