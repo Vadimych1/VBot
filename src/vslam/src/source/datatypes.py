dfcd3627735d990268c78590ed0ac4ae130f5a15
Vadimych1 <vadimlebedenko0@gmail.com>
2025-07-23 17:47:49 +0300

    upd

--- src/vslam/src/source/datatypes.py
+++ src/vslam/src/source/datatypes.py
@@ -1,5 +1,6 @@
 from miniros.util.datatypes import Datatype, Movement, Int, Vector
 import numpy as np
+import struct
 
 class SLAMMap(Datatype):
     def __init__(self, mapdata: bytearray):
@@ -17,11 +18,24 @@ class SLAMMap(Datatype):
     def to_numpy(self, mapsize: int) -> np.ndarray:
         return np.frombuffer(self.data, dtype=np.uint8).reshape((mapsize, mapsize))
 
-class SLAMPosition(Movement):
+class SLAMPosition(Datatype):
+    def __init__(self, pos: Vector, ang: Vector):
+        self.pos = pos
+        self.ang = ang
+    
     @staticmethod
-    def decode(data, decoders = { 0: Vector }):
-        q = Movement.decode(data, decoders)
-        return SLAMPosition(q.pos, q.ang)
+    def decode(data: bytes):
+        la, lb = struct.unpack(">HH", data[:4])
+        a, b = data[4:4+la], data[4+la:4+la+lb]
+        
+        return SLAMPosition(Vector.decode(a), Vector.decode(b))
+        
+    @staticmethod
+    def encode(data: "SLAMPosition"):
+        a, b = Vector.encode(data.pos), Vector.encode(data.ang)
+        la, lb = len(a), len(b)
+        
+        return struct.pack(">HH", la, lb) + a + b
 
     def pos_to_numpy(self) -> np.ndarray:
         return np.array([self.pos.x, self.pos.y, self.pos.z])
